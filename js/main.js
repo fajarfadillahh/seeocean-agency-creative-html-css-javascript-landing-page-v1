1d69ebb7c5f4e3d9160354c4f0ad8da4d0373821
fajarfadillahh <fajaragustian979@gmail.com>
2022-12-12 11:14:20 +0700

    header section

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// ===== SHOW MENU =====
+function showMenu(menuId, toggleId) {
+  const menu = document.getElementById(menuId);
+  const toggle = document.getElementById(toggleId);
+
+  if (menu && toggle) {
+    toggle.onclick = () => {
+      menu.classList.toggle("show-menu");
+    };
+  }
+}
+showMenu("header-menu", "header-toggle");
